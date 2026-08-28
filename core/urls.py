c086b5c44208800f3dda23617f3ebc03f59ff0fb
Jovany Cardenas Vargas <48363724+JovanyCardenas@users.noreply.github.com>
2026-08-28 23:59:36 +0000

    added all basic functional webpages like 404, 403, 500, ...

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,10 @@ from .views import *
 
 urlpatterns = [
     path("", home, name="home"),
-    path("about/", about, name="about"),
+    path("coming-soon/", coming_soon, name="coming_soon"),
     path("dashboard/", dashboard, name="dashboard"),
+    path("about/", about, name="about"),
+    path("contact/", contact, name="contact"),
+    path("resources/", resources, name="resources"),
+    path("settings/", settings, name="settings"),
 ]
