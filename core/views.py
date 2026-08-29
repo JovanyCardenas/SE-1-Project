a647eeb6a98cb30aa2614f6d7feb9299960155cc
Jovany Cardenas Vargas <48363724+JovanyCardenas@users.noreply.github.com>
2026-08-28 17:02:35 -0700

    added all basic functional webpages like 404, 403, 500, ... (#11)

--- core/views.py
+++ core/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+def coming_soon(request):
+    return render(request, "coming_soon.html")
+
 def home(request):
     return render(request, "pages/home.html")
 
 def about(request):
     return render(request, "pages/about.html")
+
+def contact(request):
+    return render(request, "pages/contact.html")
+
+def resources(request):
+    return render(request, "coming_soon.html")
+    # return render(request, "pages/resources.html")
+
+@login_required
+def settings(request):
+    return render(request, "coming_soon.html")
+    # return render(request, "pages/settings.html")
 
 @login_required
 def dashboard(request):
